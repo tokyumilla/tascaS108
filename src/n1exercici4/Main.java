4e602749a9d1fda540b1be5a3eae3b40d0b42b3a
tokyumilla <tokyumilla@gmail.com>
2022-11-29 18:34:18 +0100

    Ejercicio 4 hecho

---
+++ src/n1exercici4/Main.java
@@ -0,0 +1,27 @@
+package n1exercici4;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList months = new ArrayList<>();
+        months.add("enero");
+        months.add("febrero");
+        months.add("marzo");
+        months.add("abril");
+        months.add("mayo");
+        months.add("junio");
+        months.add("julio");
+        months.add("agosto");
+        months.add("septiembre");
+        months.add("octubre");
+        months.add("noviembre");
+        months.add("diciembre");
+
+        months.forEach(System.out::println);
+
+
+
+
+    }
+}
